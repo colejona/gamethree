0a6e297ae4f1b0b26115c0267c223e574ec57129
Jonathan Cole <jcole@truex.com>
2019-09-25 12:50:08 -0700

    Feature/cljs server (#1)
    * Remove yarn-related scripts
    
    * Remove server.js, add its production to build script
    
    * Heroku can't build clj -- put server.js back
    
    * Add `/out` dir -- it's required
    
    * Use process.env.PORT if it exists (for Heroku)
    
    * Serve an index.html -- nothing there yet
    
    * Fix path to empty index.html
    
    * Rename server.core to server.server

---
+++ out/server/core.js
@@ -0,0 +1,37 @@
+// Compiled by ClojureScript 1.10.520 {:target :nodejs}
+goog.provide('server.core');
+goog.require('cljs.core');
+server.core.node$module$express = require('express');
+server.core.node$module$socket$io = require('socket.io');
+server.core.node$module$path = require('path');
+cljs.core.enable_console_print_BANG_.call(null);
+cljs.core.println.call(null,"Starting server...");
+server.core.app = server.core.node$module$express.call(null);
+server.core.server = (function (){var port = (function (){var or__4131__auto__ = process.env.PORT;
+if(cljs.core.truth_(or__4131__auto__)){
+return or__4131__auto__;
+} else {
+return (5000);
+}
+})();
+return server.core.app.listen(port,((function (port){
+return (function (){
+return cljs.core.println.call(null,["Example app listening on port ",cljs.core.str.cljs$core$IFn$_invoke$arity$1(port),"!"].join(''));
+});})(port))
+);
+})();
+server.core.io = server.core.node$module$socket$io.call(null,server.core.server);
+server.core.serve_client = (function server$core$serve_client(req,resp){
+return resp.sendFile(server.core.node$module$path.resolve([cljs.core.str.cljs$core$IFn$_invoke$arity$1(__dirname),"../../../index.html"].join('')));
+});
+server.core.app.get("/",server.core.serve_client);
+server.core.io.set("origins","*:*");
+/**
+ * Sets up a new socket connection.
+ */
+server.core.setup_connection = (function server$core$setup_connection(socket){
+return cljs.core.println.call(null,"Totally setting up a new socket connection.");
+});
+server.core.io.on("connection",server.core.setup_connection);
+
+//# sourceMappingURL=core.js.map
---
+++ src/server/server.cljs
@@ -0,0 +1,32 @@
+(ns server.server
+  (:require ["express"]
+            ["socket.io" :as socket-io]
+            ["path"]))
+
+(enable-console-print!)
+
+(println "Starting server...")
+
+(def app (express))
+
+(def server
+  (let [port (or (-> js/process .-env .-PORT) 5000)]
+    (.listen app port (fn [] (println (str "Example app listening on port " port "!"))))))
+
+(def io (socket-io server))
+
+(defn serve-client
+  [req resp]
+  (.sendFile resp (.resolve path (str js/__dirname "../../../index.html"))))
+
+(.get app "/" serve-client)
+
+(.set io "origins" "*:*")
+
+(defn setup-connection
+  "Sets up a new socket connection."
+  [socket]
+  ; TODO -- this doesn't appear to be called yet
+  (println "Totally setting up a new socket connection."))
+
+(.on io "connection" setup-connection)
