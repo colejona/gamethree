3d4879c2191bd8e4bd375ef95681041e4f062324
Jonathan Cole <jcole@truex.com>
2019-10-11 10:30:20 -0700

    Merge from master

--- src/ngame/client/client.cljs
+++ src/ngame/client/client.cljs
@@ -1,4 +1,14 @@
-(ns ngame.client.client)
+(ns ngame.client.client
+  (:require ["phaser" :as phaser]))
+
+(defn main-scene [game]
+  (nth game.scene.scenes 0))
+
+(defn preload-fn []
+  (-> (main-scene js/game) .-load (.image "logo" "assets/phaser.png")))
+
+(defn create-fn []
+  (-> (main-scene js/game) .-add (.image 300 240 "logo")))
 
 (defn log
   [message]
@@ -11,7 +21,14 @@
   (start))
 
 (defn start []
-  (log "start"))
+  (log "start")
+  (set! js/game (js/Phaser.Game. (js-obj
+                                  "type" js/Phaser.AUTO
+                                  "width" 600
+                                  "height" 480
+                                  "scene" (js-obj
+                                           "preload" preload-fn
+                                           "create" create-fn)))))
 
 (defn stop []
   (log "stop"))
