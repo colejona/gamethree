97a52be0e254bd3aaa5fb44e053578d3988965c0
Isaiah Mann <isaiahbmann@gmail.com>
2019-10-10 21:22:52 -0700

    Load Phaser in client

--- src/ngame/client/client.cljs
+++ src/ngame/client/client.cljs
@@ -1,4 +1,17 @@
-(ns ngame.client.client)
+(ns ngame.client.client
+    (:require ["phaser" :as phaser]))
+
+(defn preload-fn []
+    (. (. (nth js/game.scene.scenes 0) -load) image "logo" "assets/phaser.png"))
+
+(defn create-fn []
+    (. (. (nth js/game.scene.scenes 0) -add) image 300 240 "logo"))
 
 (defn init []
-    (println "I did say hi!"))
+    (set! js/game (js/Phaser.Game. (js-obj
+        "type" js/Phaser.AUTO
+        "width" 600
+        "height" 480
+        "scene" (js-obj
+            "preload" preload-fn
+            "create" create-fn )))))
