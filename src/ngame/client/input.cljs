599af5c84a9dd3a1a29aeb89743e41fc452b8ae1
Jonathan Cole <jcole@truex.com>
2019-11-04 00:28:42 -0800

    Use doseq!

--- src/ngame/client/input.cljs
+++ src/ngame/client/input.cljs
@@ -7,9 +7,5 @@
 
 (defn setup-input [scene key-down-handler key-up-handler]
   (let [keyboard (.-keyboard (.-input scene))]
-    (def foo #(set-handlers keyboard %1 %2 key-down-handler key-up-handler))
-    ;; There has to be a better way to do this. `apply`?
-    (foo "W" :w_key)
-    (foo "A" :a_key)
-    (foo "S" :s_key)
-    (foo "D" :d_key)))
+    (doseq [[letter key] [["W" :w_key] ["A" :a_key] ["S" :s_key] ["D" :d_key]]]
+      (set-handlers keyboard letter key key-down-handler key-up-handler))))
