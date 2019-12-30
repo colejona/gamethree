e263890b32a394f5f1250ccca11ecdfc355fd1ec
Isaiah Mann <isaiahbmann@gmail.com>
2019-12-29 20:21:08 -0800

    Pick random unused position (#19)
    * Pick random unused position
    
    * Correct :y to height
    
    * Refactor to store positions by player id

--- src/ngame/server/authoritative_server/game.cljs
+++ src/ngame/server/authoritative_server/game.cljs
@@ -1,7 +1,13 @@
 (ns ngame.server.authoritative-server.game
+  (:require [ngame.common.constants :as const])
   (:use [ngame.common.phaser-util :only [main-scene add-image load-image]])
   (:require ["phaser" :as phaser])
   (:require ["datauri" :as datauri]))
+
+(defonce player-positions {})
+(defonce max-position-count
+  (* (/ const/width const/spacing)
+     (/ const/height const/spacing)))
 
 (defn preload-fn []
   (load-image js/game "player" "assets/red_square.png"))
@@ -24,12 +30,32 @@
 (defn start []
   (log "start")
   (set! js/game (js/Phaser.Game. (clj->js {:type js/Phaser.HEADLESS
-                                           :width 600
-                                           :height 480
+                                           :width const/width
+                                           :height const/height
                                            :scene {:preload preload-fn
                                                    :create create-fn
                                                    :update update-fn}
                                            :autoFocus false}))))
+
+(defn random-position []
+  {:x (* (rand-int (/ const/width const/spacing)) const/spacing)
+   :y (* (rand-int (/ const/height const/spacing)) const/spacing)})
+
+(defn position-used? [pos]
+  (contains? (set (vals player-positions)) pos))
+
+(defn new-position []
+  (let [pos (random-position)]
+    (if (position-used? pos)
+      (new-position)
+      pos)))
+
+(defn place-player! [id]
+  (let [pos (if (>= (count player-positions) max-position-count)
+              (random-position)
+              (new-position))]
+    (set! player-positions (conj player-positions {id pos}))
+    pos))
 
 (defn stop []
   (log "stop")
@@ -37,8 +63,8 @@
 
 (defn ^:export add-player
   [id player-established-callback]
-  (let [x 300
-        y 240]
+  (let [ {x :x, y :y}
+         (place-player! id) ]
     (log (str "Adding player `" id "` at " x "," y))
     (add-image js/game x y "player")
     (player-established-callback x y)))
